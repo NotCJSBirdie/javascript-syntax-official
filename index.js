bfdb830042664de0cba1cd7c7cd360612e79ddd9
Carl Serquiña <serquinajhunlo@gmail.com>
2021-10-18 09:40:04 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// This is an external javascript file
+
+function PFunction() {
+  document.getElementById("par").innerHTML = "Hello World";
+}
+
+function AFunction() {
+  let x, y;
+  x = 10;
+  y = 11;
+  document.getElementById("result").innerHTML = x + y;
+}
